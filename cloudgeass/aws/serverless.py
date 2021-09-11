faeb211665d7a618452c8a27530bb8c19cb5a0ff
ThiagoPanini <thipanini94@gmail.com>
2021-09-11 00:43:13 -0300

    Alteração da função get_packages() para download_packages() com nova lógica de definição e download de pacotes python

--- cloudgeass/aws/serverless.py
+++ cloudgeass/aws/serverless.py
@@ -32,7 +32,7 @@ Table of Contents
 # Biblitocas built-in
 import os
 import shutil
-import pathlib
+from sys import platform
 
 # Módulos auxiliares
 from cloudgeass.utils.log import log_config
@@ -54,7 +54,7 @@ logger = logging.getLogger(__file__)
 logger = log_config(logger)
 
 # Definindo variáveis fixas de uso interno
-PATH = os.path.join(os.getcwd(), 'lambda\\layer')
+PATH = os.path.join(os.getcwd(), r'lambda\layer')
 REQ_FILE = os.path.join(os.getcwd(), 'requrements.txt')
 REGION = 'sa-east-1'
 
@@ -68,7 +68,7 @@ REGION = 'sa-east-1'
 
 # Criando diretório de layer para integração com runtime do lambda
 # https://docs.aws.amazon.com/lambda/latest/dg/configuration-layers.html
-def create_layer_path(path, runtime_folder='python'):
+def create_layer_path(layers_root_path, layer_name, runtime_folder='python'):
     """
     Cria um diretório específico para o recebimento das
     dependências/pacotes python a serem inclusos no layer
@@ -108,119 +108,168 @@ def create_layer_path(path, runtime_folder='python'):
         [type: string, default='python']
     """
     
-    # Extraindo última parcela do diretório da camada
-    if os.path.isdir(path):
-        ref_folder = pathlib.PurePath(path).name
-    else:
-        ref_folder = os.path.basename(os.path.dirname(path))
-    
-    # Verificando diretório de leitura de camada no lambda
-    if ref_folder != runtime_folder:
-        path = os.path.join(path, runtime_folder)
-
     # Criando diretório
     try:
+        path = os.path.join(layers_root_path, layer_name, runtime_folder)
         os.makedirs(path)
-        logger.info(f'Diretório de camada criado em {path}')
+        logger.info(f'Diretório do layer criado em {path}')
     except FileExistsError as fee:
         logger.warning(fee)
 
     return path
 
 # Estruturando dependências do pacote
-def get_packages(layer_path, type='venv', **kwargs):
+def download_packages(target_path, packages_from='list', **kwargs):
     """
-    Coleta e estruturação de pacotes python a serem inclusos no
-    layer lambda a partir de dependências registradas manualmente
-    ou contidas no ambiente virtual de trabalho. Esta função possui
-    a grande vantagem de validar diferentes tipos de seleção de
-    dependências para a instalação direcionada dos pacotes a partir
-    do comando pip install -t apontando para o diretório do layer
-    criado a partir da função create_lambda_layer().
+    Download e estruturação de pacotes python a serem inclusos no
+    layer lambda. A grande vantagem proposta por essa função é o
+    encapsulamento dos comandos de download de pacotes a partir
+    de insumos fornecidos pelo usuário em uma instalação 
+    direcionada em um diretório alvo do layer lambda estruturado.
 
-    Existem duas abordagens de atuação para a coleta de dependências:
-        * venv: onde a coleta de dependências é feita de forma
-        automática considerando as bibliotecas instaladas no ambiente
-        virtual de trabalho ativo no momento de execução desta função
+    Na prática, existem duas possibilidades de definição da origem 
+    das dependências a serem consideradas no processo de instalação.
+    O usuário pode utilizar o parâmetro "packages_from" para essa
+    configuração, sendo as possibilidades dadas por:
 
-        * manual: onde o usuário pode inserir manualmente, de forma 
-        prévia, as dependências em um arquivo "requirements.txt" que,
-        fornecido como argumento adicional dessa função, serve como
-        insumo principal para a instalação direcionada dos pacotes
+        * 'list': nesta configuração, o usuário fornece manualmente
+        uma lista de dependências/pacotes a serem baixados dentro
+        do diretório alvo ("target_path") de construção do layer.
+        Tal lista é passada como argumento adicional dentro do
+        parâmetro "packages_list". Adicionalmente, é possível
+        configurar este formato para que o usuário preencha
+        manualmente as dependências a partir do próprio prompt
+        de comando (argumento "user_input");
 
-    Para ambos os casos, validações são realizadas nas primeiras
-    etapas de execução da função como, por exemplo, a validação
-    da existência de um arquivo de "requirements" válido quando
-    é selecionada uma abordagem de atuação "manual". Por padrão,
-    quando type="manual", a função considera que as dependências
-    estarão alocadas em um arquivo chamado "requirements.txt" 
-    localizado na pasta root do diretório de trabalho. Quando o
-    usuário seleciona type="venv", um arquivo "requirements_tmp.txt"
-    é criado de forma temporária para alocar o resultado do
-    comando pip freeze. Ao final, o arquivo "requirements_tmp.txt"
-    é excluído.
+        * 'venv': aqui, a definição das dependências/pacotes é 
+        feita de forma automática a partir da coleta dos insumos
+        já existentes no ambiente virtual ativo. Em outras palavras,
+        é criado um arquivo de requirements temporário a partir da
+        execução do comando "pip freeze > requirements_tmp.txt",
+        fazendo assim com que uma lista de dependências seja
+        gerada automaticamente considerando os insumos existentes
+        no venv de trabalho;
+
+    Após as definições dos pacotes, é realizada uma instalação
+    direcionada em um diretório alvo a partir da execução do
+    comando "python3 -m pip install -t <target_path> <package>"
 
     Parâmetros
     ----------
-    :param layer_path:
-        Referência do diretório de alocação das dependências do
-        layer a ser criado.
+    :param target_path:
+        Diretório alvo de instalação dos pacotes do layer a ser
+        estruturado. É importante considerar os requisitos necessários
+        para a criação de layers lambda de acordo com as versões
+        do python consideradas.
         [type: string]
 
-    :param type:
-        Tipo de abordagem para a coleta de dependências do layer.
-        Opções possíveis: "venv" ou "manual".
-        Detalhes fornecidos na documentação acima.
-        [type: string, default='venv']
+    :param packages_from:
+        Informação da origem dos pacotes a serem baixados e instalados
+        no diretório alvo do layer. Como mencionado acima, este
+        argumento aceita duas entradas possíveis: 'list' e 'venv'.
+        Em cada caso, diferentes regras são aplicadas para que
+        a instalação direcionada seja feita de forma adequada.
+        [type: string, default='list']
 
     Argumentos Adicionais
     ---------------------
-    :kwarg requirements_path:
-        Referência do arquivo de dependências "requirements" com a 
-        lista de pacotes a ser instalada de forma direcionada no
-        diretório alvo do layer. Caso type="venv", um arquivo
-        "requirements_tmp.txt" é criado temporariamente para servir
-        de insumo para instalação das dependências do venv. Caso
-        type="manual", é feita uma verificação da existência do
-        arquivo no diretório fornecido e, caso este argumento não
-        seja preenchido externamente na chamada da função, é 
-        considerado um arquivo de nome "requirements.txt" na raíz
-        do projeto.
-        [type: string, default=os.path.join(os.getcwd(), 'requirements.txt')]
+    :kwarg packages_list:
+        Argumento a ser preenchido caso o usuário configure a origem
+        das dependências a partir de uma lista (packages_from='list').
+        Nesta abordagem, este argumento é opcionalmente preenchido
+        com uma lista de dependências/pacotes a ser considerada no
+        ato da instalação direcionada. Internamente no código, são
+        aplicadas transformações de string (.lowere() e .strip())
+        para que o pacote seja, na medida do possível, devidamente 
+        instalado. Caso o parâmetro "packages_from" seja configurado
+        como 'list' e o usuário NÃO forneça uma lista para o 
+        argumento "packages_list", obrigatoriamente o argumento 
+        "user_input" precisa ser configurado como True para que,
+        dessa forma, o usuário tenha a chance de fornecer informações
+        dos pacotes diretamente no prompt de comando no ato de execução
+        do script de criação de layer.
+        [type: list, default=None]
+
+    :kwarg user_input:
+        Argumento a ser opcionalmente preenchido caso a configuração
+        da função seja dada com packages_from='list' e nenhuma lista
+        passada para o argumento "packages_list". Neste caso, este
+        argumento em questão trata do preenchimento manual do usuário
+        diretamente pelo prompt de comando, informando assim as 
+        dependências a serem baixadas para o layer. Assim como no caso
+        de "packages_list", este argumento considera tratativas em
+        strings para evitar, ao máximo, erros de instalação por 
+        referências de pacotes mal formatadas.
+        [type: bool, default=False]
     """
 
-    # Coletando argumentos adicionais e validando requirements
-    requirements_path = kwargs['requirements_path'] if 'requirements_path' in kwargs else os.path.join(os.getcwd(), 'requirements.txt')
-    if os.path.splitext(requirements_path)[-1] != '.txt':
-        logger.error(f'Caminho para requirements {requirements_path} inválido. Utilize um arquivo txt ou gere um via pip freeze')
+    # Validando origem das dependências
+    if packages_from == 'list':
+        logger.info(f'Pacotes a serem instalados serão fornecidos pelo usuário')
+        # Coletando argumentos adicionais pra instalação dos pacotes
+        packages_list = kwargs['packages_list'] if 'packages_list' in kwargs else None
+        user_input = bool(kwargs['user_input']) if 'user_input' in kwargs else False
+        
+        # Validando falhas na configuração do modo de coleta de pacotes
+        if packages_list is not None and type(packages_list) != list:
+            logger.error(f'Argumento "packages_list" não é uma lista. Por favor, forneça uma lista com as dependências')
+            return
+        if packages_list is None and not user_input:
+            logger.warning(f'O parâmetro "packages_from" foi configurado como "list", mas nenhuma lista foi passada como argumento da função')
+            logger.error(f'Forneça uma lista de dependências no argumento "packages_list" ou configure "user_input" como True para coleta manual no prompt')
+            return
+        elif packages_list is not None and user_input:
+            logger.warning(f'Foi passada uma lista de dependências no parâmetro "packages_list", mas o parâmetro "user_input" também foi configurado como True')
+            logger.error(f'No caso de packages_from="list", recomenda-se fornecer uma lista para o argumento packages_list OU user_input=True para evitar conflito')
+            return
+        elif packages_list is None and user_input:
+            # Coletando pacotes manualmente do usuário
+            user_pkgs = input('Insira o nome dos pacotes a serem baixados separados por vírgula: ')
+            packages_list = [pkg.lower().strip() for pkg in user_pkgs.split(',')]
+        elif packages_list is not None and not user_input:
+            # Tratando possíveis equívocos no fornecimento dos pacotes em formato de lista
+            packages_list = [pkg.lower().strip() for pkg in packages_list]
+
+        # Definindo comando básico de instalação de acordo com sistema operacional
+        if platform == 'linux':
+            base_install_cmd = f'python3 -m pip install -t {target_path} <package>'
+        else:
+            base_install_cmd = f'python3 -m pip install --no-user -t {target_path} <package>'
+        
+        # Iterando sobre cada pacote para realização da instalação
+        logger.debug(f'Instalando os {len(packages_list)} pacotes listados')
+        for pkg in packages_list:
+            pkg_install_cmd = base_install_cmd.replace('<package>', pkg)
+            os.system(pkg_install_cmd)
+        logger.info(f'Comando de instalação das dependências {packages_list} executado. Verifique no prompt se há algum erro crítico')
+
+    elif packages_from == 'venv':
+        logger.info(f'Pacotes a serem instalados serão extraídos automaticamente do ambiente virtual ativo')
+        
+        # Definindo arquivo temporário de requirements
+        req_file = os.path.join(os.getcwd(), 'requirements_tmp.txt')
+        logger.debug(f'Extraindo dependências do venv e salvando temporiamente em {req_file}')
+        os.system(f'pip freeze > {req_file}')
+
+        # Definindo comando básico de instalação de acordo com sistema operacional
+        if platform == 'linux':
+            req_install_cmd = f'python3 -m pip install -r {req_file} -t {target_path}'
+        else:
+            req_install_cmd = f'python3 -m pip install --no-user -r {req_file} -t {target_path}'
+        logger.debug(f'Instalando dependências salvas em arquivo temporário de requirements')
+        os.system(req_install_cmd)
+
+        # Eliminando arquivo temporário de requirements
+        os.remove(req_file)
+        logger.info(f'Comando para instalação das dependências do venv ativo executado. Verifique no prompt se há algum erro crítico')
+
+    else:
+        # Origem dos pacotes indefinida
+        logger.error(f'Parâmetro packages_from={packages_from} inválido. Selecione entre "list" ou "venv"')
         return
-    elif (type=='manual' and not os.path.isfile(requirements_path)):
-        logger.error(f'Arquivo {requirements_path} inexistente. Forneça um caminho válido ou gere um arquivo via pip freeze')
-        return
-
-    # Validando parâmetro de tipo de coleta
-    if type not in ('venv', 'manual'):
-        logger.error(f'Tipo de estruturação (type={type}) inválida. Por favor, selecione entre "manual" ou "venv". Programa encerrado')
-        return
-
-    # Coleta automática de pacotes extraindo do venv via pip freeze
-    if type == 'venv':
-        requirements_path = requirements_path if requirements_path in kwargs else os.path.join(os.getcwd(), 'requirements_tmp.txt')
-        logger.debug(f'Coletando pacotes presentes no ambiente virtual ativo via pip freeze e sobrescrevendo arquivo {requirements_path}')
-        os.system(f'pip freeze > {requirements_path}')
-
-    # Instalando dependências utilizando arquivo requirements.txt já existente ou recém criado
-    logger.debug(f'Realizando instalação direcionada via pip de dependências contidas em {requirements_path}\n')
-    os.system(f'pip3 install -r {requirements_path} --no-user -t {layer_path}')
-    print()
-    logger.info(f'Pacotes contidos em {requirements_path} foram disponibilizados com sucesso em {layer_path}. Verifique se o resultado no prompt contém algum erro após a chamada do pip')
-
-    # Eliminando arquivo de requirements em caso temporário
-    if requirements_path == os.path.join(os.getcwd(), 'requirements_tmp.txt'):
-        os.remove(requirements_path)
 
 # Zipando e realizando upload do layer em bucket específico
-def upload_layer_to_bucket(layer_path, bucket_name, prefix):
+def upload_layer_to_bucket(target_path, bucket_name, key):
     """
     Zipa o layer gerado a partir da instalação das dependências
     e realiza o upload do arquivo zipado para um bucket s3. 
@@ -249,23 +298,23 @@ def upload_layer_to_bucket(layer_path, bucket_name, prefix):
 
     # Zipando diretório do layer
     try:
-        shutil.make_archive(layer_path, format='zip', root_dir=layer_path)
-        logger.info(f'Diretório de dependências {layer_path} zipado com sucesso')
+        shutil.make_archive(target_path, format='zip', root_dir=target_path)
+        logger.info(f'Diretório de dependências {target_path} zipado com sucesso')
     except Exception as e:
-        logger.error(f'Erro ao zipar diretório de dependências {layer_path}. Exception: {e}')
+        logger.error(f'Erro ao zipar diretório de dependências {target_path}. Exception: {e}')
         return
 
     # Instanciando classe JimmyBuckets para operações no s3
     jbuckets = JimmyBuckets(region=REGION)
     jbuckets.upload_object(
-        file=layer_path + '.zip',
+        file=target_path + '.zip',
         bucket_name=bucket_name,
-        key=prefix + 'python.zip'
+        key=key
     )
 
 # Cria um layer e realiza o upload em um bucket s3
-def build_lambda_layer(layer_path, bucket_name, prefix, runtime_folder='python',
-                       type_get_package='venv', **kwargs):
+def build_lambda_layer(target_path, bucket_name, key, 
+                       packages_from='list', **kwargs):
     """
     Realiza a construção completa de um layer lambda a partir
     da execução individual de cada uma das etapas associadas
@@ -337,25 +386,23 @@ def build_lambda_layer(layer_path, bucket_name, prefix, runtime_folder='python',
         [type: string, default=os.path.join(os.getcwd(), 'requirements.txt')]
     """
 
-    # Criando diretório do layer
-    layer_path = create_layer_path(
-        path=layer_path,
-        runtime_folder=runtime_folder
-    )
+    # Coletando argumentos adicionais
+    packages_list = kwargs['packages_list'] if 'packages_list' in kwargs else None
+    user_input = bool(kwargs['user_input']) if 'user_input' in kwargs else False 
 
-    # Coletando dependências
-    requirements_path = kwargs['requirements_path'] if 'requirements_path' in kwargs else os.path.join(os.getcwd(), 'requirements.txt')
-    get_packages(
-        layer_path=layer_path,
-        type=type_get_package,
-        requirements_path=requirements_path
+    # Coletando e baixando dependências no diretório alvo
+    download_packages(
+        target_path=target_path,
+        packages_from=packages_from,
+        packages_list=packages_list,
+        user_input=user_input
     )
 
     # Zipando layer e subindo pra bucket s3
     upload_layer_to_bucket(
-        layer_path=layer_path,
+        target_path=target_path,
         bucket_name=bucket_name,
-        prefix=prefix
+        key=key
     )
 
     
