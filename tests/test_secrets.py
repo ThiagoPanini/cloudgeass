1848d1e5405aed8cd36f17015d091e976af4c3a5
Thiago Panini <panini.development@gmail.com>
2023-09-18 23:37:19 -0300

    test: including test cases for get_secret_string() method

--- tests/test_secrets.py
+++ tests/test_secrets.py
@@ -35,3 +35,23 @@ def test_get_secret_string_method_retrieves_the_expected_secret_string(
     secret_string = sm.get_secret_string(secret_id=MOCKED_SECRET_NAME)
 
     assert secret_string == MOCKED_SECRET_VALUE
+
+
+@pytest.mark.secrets
+@pytest.mark.get_secret_string
+@mock_secretsmanager
+def test_error_on_trying_to_retrieve_a_secret_string_with_an_invalid_secret_id(
+    sm, prepare_mocked_secrets
+):
+    """
+    G: Given that users want to retrieve a secret string from a secret ID
+    W: When the method get_secret_string() is called from SecretsManagerClient
+       with an invalid secret id (i.e. a secret ID that doesn't exists)
+    T: Then a exception must be thrown
+    """
+
+    # Preparing a mocked Secrets Manager environment with mocked secrets
+    prepare_mocked_secrets()
+
+    with pytest.raises(Exception):
+        _ = sm.get_secret_string(secret_id="some-invalid-secret-id")
